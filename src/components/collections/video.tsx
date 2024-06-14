f33240ffd1071d5a5e3c1f87cb3e20e224c97ad5
Eldar <random24@yandex.ru>
2024-06-14 14:54:38 -0400

    fixed autoplay
    fixed AutoPlay

--- src/components/collections/video.tsx
+++ src/components/collections/video.tsx
@@ -11,6 +11,9 @@ export default function Video({
     return (
       <div className={`rounded-lg border shadow-md m-1 flex flex-col justify-start p-4 ${openClose?"lg:h-[calc(50vh-5px)]":"lg:h-[calc(100vh-69px)]"} backdrop-blur-sm backdropFilter lg:backdrop-blur-none duration-200`}>
         <video  controls
+                playsInline
+                autoPlay
+                muted
                 preload="metadata"
                 key={choosenVideo}
                 className="w-[100%] h-[100%] lg:max-h-[calc(58vw-265px)] bg-white">
