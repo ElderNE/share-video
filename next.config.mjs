a64acb8fe46b8e22f3cd60a9ac834d6ed6491c68
Eldar <random24@yandex.ru>
2024-06-17 22:09:38 -0400

    fix
    fix

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,21 @@
 
 const nextConfig = {
     env: {
-        END_POINT_VIDEOS:   "https://take-home-assessment-423502.uc.r.appspot.com/api/videos",
-        END_POINT_COMMENTS: "https://take-home-assessment-423502.uc.r.appspot.com/api/videos/comments"
-    }
+        END_POINT_VIDEOS:   "/api/videos",
+        END_POINT_COMMENTS: "/api/videos/comments"
+    },
+    async rewrites() {
+        return [
+          {
+            source: '/api/videos',
+            destination: 'https://take-home-assessment-423502.uc.r.appspot.com/api/videos',
+          },
+          {
+            source: '/api/videos/comments',
+            destination: 'https://take-home-assessment-423502.uc.r.appspot.com/api/videos/comments',
+          },
+        ]
+      },
 };
 
 export default nextConfig;
