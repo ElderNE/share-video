25af576663a2cffd1bf87534a6d9e56dfcd9e5d9
Eldar <random24@yandex.ru>
2024-06-17 20:15:38 -0400

    next
    next

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 
 const nextConfig = {
     env: {
-        END_POINT_VIDEOS: "https://take-home-assessment-423502.uc.r.appspot.com/api/videos",
+        END_POINT_VIDEOS:   "https://take-home-assessment-423502.uc.r.appspot.com/api/videos",
         END_POINT_COMMENTS: "https://take-home-assessment-423502.uc.r.appspot.com/api/videos/comments"
     }
 };
